385c21d7b4c972c887be32f988f05278e03d1826
Ryan Diaz <ryandiaz@gmail.com>
2018-08-06 22:37:30 -0400

    Styling is currently broken, fix incoming

--- client/src/App.js
+++ client/src/App.js
@@ -1,83 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Signup from './pages/signup';
 import Signin from './pages/Signin';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-
-const routes = [   // Define the various components that need to be rendered into an array
-  { path: '/',
-    exact: true,
-    nav: () => <Nav />,
-    home: () => <Home />,
-    footer: () => <Footer />
-  },
-  {
-    path: '/signup',
-    exact: true,
-    nav: () => <Nav />,
-    signup: () => <Signup />,
-    footer: () => <Footer />
-  },
-  {
-    path: '/signin',
-    exact: true,
-    nav: () => <Nav />,
-    signin: () => <Signin />,
-    footer: () => <Footer />
-  }
-]
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <header>
-            {routes.map((route, index) => (   // Access the routes array from above and render the Nav component
-              <Route
-                key={index}
-                path={route.path}
-                exact={route.exact}
-                component={route.nav}
-                />
-            ))}
-            {routes.map((route, index) => (   // Access the routes array from above and render the Home component
-              <Route
-                key={index}
-                path={route.path}
-                exact={route.exact}
-                component={route.home}
-              />
-            ))}
-            {routes.map((route, index) => (   // Access the routes array from above and render the Signup component
-              <Route
-                key={index}
-                path={route.path}
-                exact={route.exact}
-                component={route.signup}
-              />
-            ))}
-            {routes.map((route, index) => (   // Access the routes array from above and render the signin component
-              <Route
-                key={index}
-                path={route.path}
-                exact={route.exact}
-                component={route.signin}
-              />
-            ))}
-          </header>
-          <footer className="footer text-center">
-            {routes.map((route, index) => (   // Access the routes array from above and render the Home component
-              <Route
-                key={index}
-                path={route.path}
-                exact={route.exact}
-                component={route.footer}
-              />
-            ))}
-          </footer>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/signin' component={Signin} />
+            <Route path='/signup' component={Signup} />
+          </Switch>
         </div>
       </Router>
     );
