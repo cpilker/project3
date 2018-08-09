4e6ff4636af6ca9ad621f6e15df749dd0e1c27c4
Ryan Diaz <ryandiaz@gmail.com>
2018-08-09 12:08:49 -0400

    Signup now returns success or error message

--- routes/api/passport-routes.js
+++ routes/api/passport-routes.js
@@ -8,23 +8,26 @@ module.exports = function(app) {
     console.log(req.body);
     User.register(new User(
       { 
-        username: req.body.newemail,
+        username: req.body.username,
         firstname: req.body.newfirstname,
         lastname: req.body.newlastname,
         address1: req.body.newaddress1,
         address2: req.body.newaddress2,
         city: req.body.newcity,
         state: req.body.newstate,
-        zip: req.body.newzip
-      }), req.body.newpassword, function(err, account) {
+        zip: req.body.newzip,
+        password: req.body.password
+      }), req.body.password, function(err, account) {
       if (err) {
+        console.log("error found in passport-routes.js!");
           console.log(err);
+          res.json({err});
       } else {
           console.log('New user added!');
           passport.authenticate('local')(req, res, function() {
+            console.log(req.user);
             console.log('Done!');
-            console.log(req);
-            res.json({username: req.username});
+            res.json({username: req.user.username});
           });
         }
       });
