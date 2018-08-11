0c65873dff74a99bb7db31a7779e458d832219b9
Ryan Diaz <ryandiaz@gmail.com>
2018-08-11 00:02:23 -0400

    Create My Profile now works with file upload

--- routes/api/passport-routes.js
+++ routes/api/passport-routes.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
           if (err) {
             return reject(err);
           }
-          const filename = 'testing123';
+          const filename = req.body.username[0];
           const fileInfo = {
             filename: filename,
             bucketName: 'uploads',
@@ -35,19 +35,20 @@ module.exports = function(app) {
 
     console.log("Signup post incoming...");
     console.log(req.body);
+    console.log(req.body.username[0]);
     
     User.register(new User(
       { 
-        username: req.body.username,
-        firstname: req.body.newfirstname,
-        lastname: req.body.newlastname,
-        address1: req.body.newaddress1,
-        address2: req.body.newaddress2,
-        city: req.body.newcity,
-        state: req.body.newstate,
-        zip: req.body.newzip,
-        password: req.body.password
-      }), req.body.password, function(err, account) {
+        username: req.body.username[0],
+        firstname: req.body.newfirstname[0],
+        lastname: req.body.newlastname[0],
+        address1: req.body.newaddress1[0],
+        address2: req.body.newaddress2[0],
+        city: req.body.newcity[0],
+        state: req.body.newstate[0],
+        zip: req.body.newzip[0],
+        password: req.body.password[0]
+      }), req.body.password[0], function(err, account) {
       if (err) {
         console.log("error found in passport-routes.js!");
           console.log(err);
