9e4920e2e331be4b0ebe4126c6c25f39ef886e47
Ryan Diaz <ryandiaz@gmail.com>
2018-08-20 23:09:04 -0400

    Signin page styling complete

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,14 +1,16 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import './Footer.css';
 
 class Footer extends Component {
   render () {
     return (
-      <div className="container text-muted" id="footer">
-          Find us at: <a href="https://www.facebook.com/recruithound.io" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f text-muted"></i></a>
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in text-muted"></i></a>
-          <a href="https://twitter.com/recruit_hound" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter text-muted"></i></a>
-      </div>      
+      <Fragment>
+          <div className="container text-muted text-center" id="footer">
+            Find us at: <a href="https://www.facebook.com/recruithound.io" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f text-muted"></i></a>
+            <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in text-muted"></i></a>
+            <a href="https://twitter.com/recruit_hound" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter text-muted"></i></a>
+          </div>
+      </Fragment>
     )
   }
 }
