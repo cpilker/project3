25b07c2e2af4edaadc5b5c98058169169048e1f7
Matt <matthewgeddes@yahoo.com>
2018-08-21 00:21:14 -0400

    switched to ajax call for uploading picture

--- client/src/components/ProfilePic/ProfilePic.js
+++ client/src/components/ProfilePic/ProfilePic.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import $ from 'jquery'
 
 
 class ProfilePic extends Component {
@@ -6,16 +7,52 @@ class ProfilePic extends Component {
     default: "Hello World"
   }
 
+ 
+  uploadPicture () {
+
+    let data = new FormData();    
+    data.append('file', $('#file1')[0].file);
+
+    $.ajax({
+      url: `/upload/${this.props.id}/profilePic`,
+      data: data,
+      processData: false,
+      contentType: false,
+      type: 'POST',
+      success: (data) => {
+        if (data.err) {
+          console.log("Error!");
+          console.log(data.err);
+          this.setState({
+            errorMessage: data.err.message
+          })
+        } else {
+
+        }
+      },
+      error: (err) => {
+        console.log(err);
+        this.setState({
+          errorMessage: err.statusText
+        })
+      }
+    });
+  }
+
   render () {
     return (
     <div style={{width: '250px', position: 'relative'}} >
 
-      <form action="/upload" method='POST' encType='multipart/form-data'>
-        <div className='custom-file mb-3'>
-          <input type='hidden' name='file' value={this.props.id} />
-          <input type='hidden' name='file' value='profilePic' />
+
+      <form action={`/upload/${this.props.id}/profilePic`} method='POST' encType='multipart/form-data'>
           <input type='file' name='file' id='file' className='custom-file-input'/>
-          <input type="hidden" name="id" value={this.props.id} />
+          <label htmlFor='file1' className='custom-file-label'> </label>
+          <input type='submit' value='TESTING' className='' onClick={this.uploadPicture}/>
+      </form>
+
+      <form action={`/upload/${this.props.id}/profilePic`} method='POST' encType='multipart/form-data'>
+        <div className='custom-file mb-3'>
+          <input type='file' name='file' id='file' className='custom-file-input'/>
           <label htmlFor='file' className='custom-file-label'> </label>
         </div>
         <input type='submit' value='Save' className=''/>
@@ -31,7 +68,7 @@ class ProfilePic extends Component {
       </form>
 
       <div style={{width: '250px', height: '250px', overflow: 'hidden'}} > 
-        <img style={{width: '250px'}} src={"image/" + this.props.id + "/profilePic"} alt=''/>
+        <img style={{width: '250px'}} src={`image/${this.props.id}/profilePic`} alt=''/>
       </div>
 
 
