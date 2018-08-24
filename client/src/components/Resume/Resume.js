7932555dc9bac3eeb7d49fa2aba0f12a102a15e0
Unknown <saxton.christian@gmail.com>
2018-08-24 15:38:17 -0400

    Added styling to resume upload

--- client/src/components/Resume/Resume.js
+++ client/src/components/Resume/Resume.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-
+import './Resume.css';
 
 class Resume extends Component {
   state = {
@@ -8,26 +8,27 @@ class Resume extends Component {
 
   render () {
     return (
-    <div style={{width: '250px', position: 'relative'}} >
+    <div id="resumeBuilder">
+      <h2>Resume</h2>
 
       <form action="/upload" method='POST' encType='multipart/form-data'>
-        <div className='custom-file mb-3'>
+        <div className='custom-file'>
           <input type='hidden' name='file' value={this.props.id} />
           <input type='hidden' name='file' value='Resume' />
           <input type='file' name='file' id='file' className='custom-file-input'/>
           <input type="hidden" name="id" value={this.props.id} />
-          <label htmlFor='file' className='custom-file-label'> </label>
+          <label htmlFor='file' className='custom-file-label browseStyle'> </label>
         </div>
-        <input type='submit' value='Save' className=''/>
+        <input type='submit' value='Save' className='btn btn-primary'/>
       </form>
 
-      <form action={`/files/${this.props.id}/Resume?_method=DELETE`} method='POST' > 
-        <input type='submit' value='Delete' className=''/>
-      </form>
+        <form action={`/files/${this.props.id}/Resume?_method=DELETE`} method='POST' > 
+          <input type='submit' value='Delete' className='btn btn-primary'/>
+        </form>
 
-      <form action={`/download/${this.props.id}/Resume?_method=GET`} method='POST' > 
-        <input type='submit' value='Download' className=''/>
-      </form>
+        <form action={`/download/${this.props.id}/Resume?_method=GET`} method='POST' > 
+          <input type='submit' value='Download' className='btn btn-primary'/>
+        </form>
 
       <div style={{width: '250px', height: '250px', overflow: 'hidden'}} > 
         <img style={{width: '250px'}} src={"image/" + this.props.id + "/Resume"} alt=''/>
